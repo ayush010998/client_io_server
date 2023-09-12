77ad5c6527fc3afbabcf12741dfe77bff9a2b786
Ayush Anand <ayushanand428@gmail.com>
2023-09-12 13:38:03 -0700

    dead

---
+++ index.js
@@ -0,0 +1,31 @@
+const http=require('http');
+
+const express=require('express');
+
+const path=require('path');
+
+const Server=require("socket.io");
+
+const app=express();
+
+const server=http.createServer(app);
+
+const io=new Server(server);
+
+
+io.on('connection',(socket)=>{
+    socket.on('user-message',(message)=>{
+        console.log("a new user message",message);
+    })
+})
+
+app.use(express.static(path.resolve('./public')));
+
+
+app.get("/",(req,res)=>{
+    res.sendFile("./public/index.html")
+})
+
+server.listen(9000,()=>{
+    console.log("server started at port 9000");
+})
